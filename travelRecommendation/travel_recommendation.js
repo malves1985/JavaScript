88f3b2210e49db16379e3a1c9a2bab5a8bcac170
maalves1985 <mario.alves@adp.pt>
2025-02-26 14:22:06 -0500

    v2

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -1,54 +1,83 @@
 
-function ReadJSON() {
-    const search = document.getElementById('search').value;
+const btnSearch = document.getElementById("btnSearch");
+const btnClear = document.getElementById("btnClear");
+
+function hide(obj) {
+    var home = document.getElementById('home');
+    home.style.display = 'none';
+
+    var contact = document.getElementById('contact');
+    contact.style.display = 'none';
+
+    var about = document.getElementById('about');
+    about.style.display = 'none';
+
+    var el = document.getElementById(obj);
+    el.style.display = 'block';
+}
+
+function Clear(event) {
+    const contentList = document.getElementById('contentList');
+    contentList.innerHTML = '';
+}
+
+function ReadJSON(event) {
+    event.preventDefault();
+    const search = document.getElementById('search').value.toLowerCase();
     const locals = [];
 
     fetch('./travel_recommendation_api.json').then(response => {
         return response.json();
     }).then(data => {
         //console.log(data);
-        data.countries.forEach(element => {
-            //console.log(element);
-            element.cities.forEach(citie => {
-                //console.log(citie.name);
-                if(citie.name.toLowerCase().includes('sydney'))
+        //console.log(search);
+        if (search != '')
+        {
+            data.countries.forEach(element => {
+                //console.log(element);
+                element.cities.forEach(citie => {
+                    //console.log(citie.name);
+                    if(citie.name.toLowerCase().includes(search) || citie.description.toLowerCase().includes(search))
+                    {
+                        locals.push(citie);
+                    }
+                })
+            });
+            data.temples.forEach(element => {
+                if(element.name.toLowerCase().includes(search) || element.description.toLowerCase().includes(search))
                 {
                     locals.push(citie);
                 }
-            })
-        });
-        data.temples.forEach(element => {
-            if(element.name.toLowerCase().includes('sydney'))
-            {
-                locals.push(citie);
-            }
-        });
-        data.beaches.forEach(element => {
-            if(element.name.toLowerCase().includes('sydney'))
-            {
-                locals.push(citie);
-            }
-        });
-        //console.log(locals);
-        const content2 = document.getElementById('content2');
-        content2.innerHTML = '';
+            });
+            data.beaches.forEach(element => {
+                if(element.name.toLowerCase().includes(search) || element.description.toLowerCase().includes(search))
+                {
+                    locals.push(citie);
+                }
+            });
+            //console.log(locals);
+            const contentList = document.getElementById('contentList');
+            contentList.innerHTML = '';
 
-        locals.forEach(element => {
-            console.log(element.name);
-            console.log(element.description);
-            content2.innerHTML = `<img src="${element.imageUrl}" style="width:800px;height:500px;">
-                <div id='localDesc'>
-                    <h1 style="padding-left: 20px;">${element.name}</h1>
-                    <h3 style="padding-left: 20px;">${element.description}</h3>
-                </div>
-                </br>`;
-        });
+            locals.forEach(element => {
+                var div = document.createElement('div');
+                div.innerHTML = `<img src="${element.imageUrl}" style="width:700px;height:400px;">
+                    <div id='localDesc'>
+                        <h1 style="padding-left: 20px;">${element.name}</h1>
+                        <h3 style="padding-left: 20px;">${element.description}</h3>
+                    </div>
+                    </br>`;
 
-        
+                document.getElementById('contentList').appendChild(div);
+            });
+        }
+        else
+        Clear();
 
     }).catch(err => {
         console.log(err);
     });
 };
 
-ReadJSON();
+btnSearch.addEventListener("click", ReadJSON);
+btnClear.addEventListener("click", Clear);
